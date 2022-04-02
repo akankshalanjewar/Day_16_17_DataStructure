bf9094508f061bc63bf32693282fc5c930f74de4
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-04-02 13:56:34 +0530

    [Akanksha]: prime number program added

---
+++ primeNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace primeNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare the variables
+            int a, b, i, j, flag;
+
+            Console.WriteLine("Enter First Value: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEnter Second Value: ");
+            b = int.Parse(Console.ReadLine());
+
+            // Print display message
+            Console.WriteLine("\n Prime numbers between " +"{0} and {1} are:",a,b);
+                              
+
+            for (i = a; i <= b; i++)
+            {
+                if (i == 1 || i == 0)
+                    continue;
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                // flag = 1 means i is prime
+                // and flag = 0 means i is not prime
+                if (flag == 1)
+                    Console.WriteLine(i);
+            }
+        }
+
+    }
+}
+ 
+
